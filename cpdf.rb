cc837f17cb72c0bb476f4a5fd9301fb0d832b68d
Thomas Parisot <oncletom@users.noreply.github.com>
2017-09-27 14:27:59 +0100

    Fix version number

--- cpdf.rb
+++ cpdf.rb
@@ -2,7 +2,7 @@ class Cpdf < Formula
   desc "Coherent PDF Command Line Tools give you a wide range of professional, robust tools to modify PDF files."
   homepage "https://www.coherentpdf.com/"
   url "https://github.com/coherentgraphics/cpdf-binaries/raw/8e308f25a329e6ac3728a69afdc1ef531a24767c/OSX-Intel/cpdf"
-  version "V2.2 Patchlevel 1"
+  version "2.2.1"
   sha256 "4bb1893067e44d0aa8debda1ec1f24d3239c690c26fe950d79a5e3eadc5c723a"
 
   bottle :unneeded
