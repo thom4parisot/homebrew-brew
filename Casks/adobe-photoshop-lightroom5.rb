fd09e7b44304e4b921fe8b9f674b2cc58eba6add
Thomas Parisot <oncletom@users.noreply.github.com>
2019-03-30 15:57:36 +0100

    Update adobe-photoshop-lightroom5.rb

--- Casks/adobe-photoshop-lightroom5.rb
+++ Casks/adobe-photoshop-lightroom5.rb
@@ -3,7 +3,8 @@ cask 'adobe-photoshop-lightroom5' do
   sha256 '155a91e2c90927a05ccaa244a99fed4784fa7cf26d08c634f5f111629f6b0418'
 
   url "http://trials3.adobe.com/AdobeProducts/LTRM/#{version.gsub(".", "_")}/osx10/Lightroom_#{version.major}_CCM_LS11.dmg",
-      user_agent: :fake
+      user_agent: :fake,
+      cookies: { 'MM_TRIALS' => '1234' }
   name 'Adobe Photoshop Lightroom 5'
   homepage 'https://helpx.adobe.com/fr/download-install/kb/lightroom-downloads.html'
 
