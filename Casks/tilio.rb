6a3d58333d73326f92f053b3159d09099bb0e1dc
Thomas Parisot <oncletom@users.noreply.github.com>
2019-10-11 15:19:35 +0200

    tilio@1.6.1

--- Casks/tilio.rb
+++ Casks/tilio.rb
@@ -1,8 +1,8 @@
 cask 'tilio' do
-  version '1.0.0-rc97'
-  sha256 '41aeb79034d0f06fdcbc75c353c05c80eada0dff616f2c9769200811e1f11318'
+  version '1.6.1'
+  sha256 '652a35335d38503c886ab024f445ec8b6903edf5e58a24775f7fd31802159f83'
 
-  url "https://tilio-nucleus.s3.eu-west-2.amazonaws.com/Tilio/1690bd10307c9c3b87d474ab67c1c63f/darwin/x64/Tilio-#{version}.mac.dmg"
+  url "https://tilio-nucleus.s3.eu-west-2.amazonaws.com/Tilio/1690bd10307c9c3b87d474ab67c1c63f/darwin/x64/Tilio-#{version}.dmg"
   name 'Tilio'
   homepage 'https://tilio.app/'
 
